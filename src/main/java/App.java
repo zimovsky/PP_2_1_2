8464d9073a0b9e5e9fc0d7a6b1251f22a79b3ace
Sergey Zimovsky <sergey@zimovsky.pro>
2024-03-31 02:28:46 +0300

    Try to pass the test
    - Read about Bean types
    - Create a POJO class Cat
    - Create a cat bean in AppConfig
    - Change its scope to prototype
    - Make changes to App class
    - Uncomment test class and give it a try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(bean3.equals(bean4));
     }
 }
